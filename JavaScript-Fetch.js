71048e439ef962fca7f17263e02f5bcdcf0e1ae9
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-03 21:58:32 +0500

    Update JavaScript-Fetch.js

--- JavaScript-Fetch.js
+++ JavaScript-Fetch.js
@@ -9,3 +9,21 @@ const getData = async () => {
 }
 getData()
 
+const postData = async () => {
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify({
+			firstName: 'Ahsan',
+			lastName: 'Shakeel'
+		}),
+		headers: {
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+	console.log(response)
+
+	const data = await response.json()
+	console.log(data)
+
+}
+postData()
